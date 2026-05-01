968191c2999062ab7c80b5cadb6c053bb353e0f9
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-05-01 00:28:48 -0300

    Add Cadastro component for registration page

---
+++ src/Cadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cadastro = () => {
+  return (
+    <div style={{ color: 'white', padding: '50px' }}>
+      <h1>Página de Cadastro</h1>
+      <p>Em breve...</p>
+    </div>
+  );
+};
+
+export default Cadastro;
+
