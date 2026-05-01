30513fb7f92b319914f8de2abad0c82c500cd75a
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-05-01 01:47:59 -0300

    Import CadastroProduto component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Cadastro from './Cadastro';
+import CadastroProduto from './CadastroProduto'; // <--- ADICIONE ESTA LINHA AQUI!
 
 function App() {
   return (
