56fefc08e64a0ec36d3a9a8a4dc88d6e278968b6
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-05-01 00:45:16 -0300

    Implement routing with React Router
    Refactored main.jsx to use React Router for navigation.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Cadastro from './Cadastro';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Cadastro />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
