11dfafade5e22604fca20c02d5b9acf55d640445
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-19 20:19:24 -0300

    Add protected routes for admin and product registration

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './paginas/cliente/Home';
 import Cadastro from './paginas/auth/Cadastro';
 import CadastroProduto from './paginas/loja/CadastroProduto';
+import PainelLoja from './paginas/loja/PainelLoja';
+import RotaProtegida from './componentes/RotaProtegida';
 
 function App() {
   return (
@@ -11,7 +13,22 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Cadastro mode="login" />} />
         <Route path="/registrar" element={<Cadastro mode="registrar" />} />
-        <Route path="/admin/cadastrar" element={<CadastroProduto />} />
+        <Route
+          path="/admin"
+          element={
+            <RotaProtegida tipoPermitido="loja">
+              <PainelLoja />
+            </RotaProtegida>
+          }
+        />
+        <Route
+          path="/admin/cadastrar"
+          element={
+            <RotaProtegida tipoPermitido="loja">
+              <CadastroProduto />
+            </RotaProtegida>
+          }
+        />
       </Routes>
     </Router>
   );
