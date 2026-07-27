11ad2894e3a89fdf71fbc6b346181f937015b164
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-26 23:32:25 -0300

    Add router configuration with protected routes

---
+++ src/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import Landing from "./paginas/Landing";
+import Login from "./paginas/auth/Login";
+import Cadastro from "./paginas/auth/Cadastro";
+import EscolherPapel from "./paginas/auth/EscolherPapel";
+import { RotaProtegida } from "./componentes/layout/RotaProtegida";
+import Home from "./paginas/cliente/Home";
+import DashboardLoja from "./paginas/loja/DashboardLoja";
+import CatalogoBaterias from "./paginas/loja/CatalogoBaterias";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Landing /> },
+  { path: "/login", element: <Login /> },
+  { path: "/cadastro", element: <Cadastro /> },
+  { path: "/escolher-papel", element: <EscolherPapel /> },
+  {
+    path: "/painel",
+    element: (
+      <RotaProtegida rolesPermitidos={["cliente"]}>
+        <Home />
+      </RotaProtegida>
+    ),
+  },
+  {
+    path: "/loja",
+    element: (
+      <RotaProtegida rolesPermitidos={["loja"]}>
+        <DashboardLoja />
+      </RotaProtegida>
+    ),
+  },
+  {
+    path: "/loja/catalogo",
+    element: (
+      <RotaProtegida rolesPermitidos={["loja"]}>
+        <CatalogoBaterias />
+      </RotaProtegida>
+    ),
+  },
+]);
